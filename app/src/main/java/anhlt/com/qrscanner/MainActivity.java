73b0e00b91b9b6550981d41909e84354851ac876
tuananh <anh.le.tuan@ntq-solution.com.vn>
2018-05-02 09:53:09 +0700

    * Fix permission request

--- app/src/main/java/anhlt/com/qrscanner/MainActivity.java
+++ app/src/main/java/anhlt/com/qrscanner/MainActivity.java
@@ -1,30 +1,26 @@
 package anhlt.com.qrscanner;
 
-import android.content.pm.PackageManager;
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.WindowManager;
 
-import com.google.zxing.Result;
-import com.google.zxing.ResultMetadataType;
-
 import anhlt.com.qrscanner.Utilities.PermissionUtil;
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     public NavigationManager navigationManager;
+
+    public static final int REQUEST_CODE_CAMERA = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         navigationManager = new NavigationManager(getSupportFragmentManager(), R.id.fragment_container);
         setContentView(R.layout.activity_main);
-        String requiredPermissions[] = new String[]{PackageManager.FEATURE_CAMERA};
-        PermissionUtil.requestPermissions(this, requiredPermissions, 0);
+        String requiredPermissions[] = new String[]{Manifest.permission.CAMERA};
+        PermissionUtil.requestPermissions(this, requiredPermissions, REQUEST_CODE_CAMERA);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
         navigationManager.addPage(new ScanViewFragment());
     }
 }
