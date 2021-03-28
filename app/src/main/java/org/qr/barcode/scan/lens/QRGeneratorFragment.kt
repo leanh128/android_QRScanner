157be40123eda3cdf5ea00ae92438258003b24c2
Tuan Anh <anhletuan128@gmail.com>
2021-03-28 14:57:00 +0700

    - Release 7 (2.1.1) - Hot fix share feature

--- app/src/main/java/org/qr/barcode/scan/lens/QRGeneratorFragment.kt
+++ app/src/main/java/org/qr/barcode/scan/lens/QRGeneratorFragment.kt
@@ -4,8 +4,8 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.os.Environment
 import android.os.Handler
 import android.os.Looper
 import android.util.DisplayMetrics
@@ -17,6 +17,7 @@ import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.FileProvider
 import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import com.google.zxing.BarcodeFormat
@@ -58,23 +59,18 @@ class QRGeneratorFragment : Fragment() {
         }
 
         binding.imgShare.setOnClickListener {
-            binding.imgQrCode.drawable?.toBitmap()?.let { bitmap ->
+            binding.imgQrCode.drawable?.toBitmap()?.let {
                 try {
-                    val fileName = "qr-code.jpg"
-                    val tempFile = File(it.context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), fileName)
-                    val out = FileOutputStream(tempFile)
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
-                    out.flush()
-                    out.close()
+                    val generatedFile = File(activity?.getExternalFilesDir(null), "$SHARE_DIR/$GENERATED_FILE_NAME")
                     val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                        Log.d("leon", "generated file: ${generatedFile.absolutePath}")
+                        putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(binding.root.context, "${BuildConfig.APPLICATION_ID}.file_provider", generatedFile))
                         type = "image/jpeg"
-                        putExtra(Intent.EXTRA_STREAM, Uri.fromFile(tempFile))
-//                        putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(it.context, "${BuildConfig.APPLICATION_ID}.file_provider", tempFile))
                         addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
                     }
                     startActivity(Intent.createChooser(shareIntent, "Sharing QR code"))
                 } catch (e: Exception) {
-                    Toast.makeText(it.context, "IO Error", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(activity, "IO Error", Toast.LENGTH_SHORT).show()
                     e.printStackTrace()
                 }
             }
@@ -94,30 +90,29 @@ class QRGeneratorFragment : Fragment() {
         if (lastGenerateText == content)
             return
         activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        val displayMetrics = DisplayMetrics()
-        activity?.display?.getRealMetrics(displayMetrics)
 //        val generateSize = (displayMetrics.heightPixels.coerceAtLeast(200.dpToPx()).coerceAtMost(displayMetrics.widthPixels.coerceAtLeast(200.dpToPx())) *.8).toInt()
-        val generateSize = 400
         var codeMatrix: BitMatrix? = null
         try {
-            codeMatrix = QRCodeWriter().encode(content, BarcodeFormat.QR_CODE, generateSize, generateSize, mapOf(
+            codeMatrix = QRCodeWriter().encode(content, BarcodeFormat.QR_CODE, BITMATRIX_SIZE, BITMATRIX_SIZE, mapOf(
                     EncodeHintType.MARGIN to 1
             ))
         } catch (e: WriterException) {
             Toast.makeText(binding.root.context, "IO Error", Toast.LENGTH_SHORT).show()
         }
         codeMatrix?.let {
-            val bmp = Bitmap.createBitmap(generateSize, generateSize, Bitmap.Config.RGB_565)
-            for (x in 0 until generateSize) {
-                for (y in 0 until generateSize) {
+            val bmp = Bitmap.createBitmap(BITMATRIX_SIZE, BITMATRIX_SIZE, Bitmap.Config.RGB_565)
+            for (x in 0 until BITMATRIX_SIZE) {
+                for (y in 0 until BITMATRIX_SIZE) {
                     bmp.setPixel(x, y, if (it.get(x, y)) Color.BLACK else Color.WHITE)
                 }
             }
-
-            val  targetSize = (floor((displayMetrics.heightPixels.coerceAtLeast(200.dpToPx()).coerceAtMost(displayMetrics.widthPixels.coerceAtLeast(200.dpToPx())) * .9f) / generateSize) * generateSize).toInt()
             hideKeyboard()
+            val displaySize = calculateQRDisplaySize()
+            val displayBitmap = Bitmap.createScaledBitmap(bmp, displaySize, displaySize, false)
+            writeImageToFile(displayBitmap)
+
+            binding.imgQrCode.setImageBitmap(displayBitmap)
             Handler(Looper.getMainLooper()).postDelayed({
-                binding.imgQrCode.setImageBitmap(Bitmap.createScaledBitmap(bmp, targetSize, targetSize, false))
                 (activity as? MainActivity)?.scrollToBottom()
                 binding.imgShare.visibility = View.VISIBLE
             }, 200)
@@ -125,9 +120,36 @@ class QRGeneratorFragment : Fragment() {
         }
     }
 
+    private fun calculateQRDisplaySize(): Int {
+        val displayMetrics = DisplayMetrics()
+        activity?.display?.getRealMetrics(displayMetrics)
+
+        val width = DEFAULT_QR_DISPLAY_SIZE_DP.dpToPx().coerceAtLeast(displayMetrics.widthPixels - 32f.dpToPx())
+        val height = DEFAULT_QR_DISPLAY_SIZE_DP.dpToPx().coerceAtLeast(displayMetrics.heightPixels - 32f.dpToPx())
+        return (floor(width.coerceAtMost(height) / BITMATRIX_SIZE) * BITMATRIX_SIZE).toInt()
+    }
+
+    private fun writeImageToFile(bitmap: Bitmap) {
+        val sharedDir = File(activity?.getExternalFilesDir(null), SHARE_DIR)
+        sharedDir.mkdirs()
+        val tempFile = File(sharedDir, GENERATED_FILE_NAME);
+        tempFile.delete()
+        val out = FileOutputStream(tempFile)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
+        out.flush()
+        out.close()
+    }
+
     override fun onStop() {
         super.onStop()
         Log.d("leon", "stop gen")
         activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+
+    companion object {
+        private const val BITMATRIX_SIZE = 400
+        private const val DEFAULT_QR_DISPLAY_SIZE_DP = 200f
+        private const val GENERATED_FILE_NAME = "qr_code.jpg"
+        private const val SHARE_DIR = "share"
     }
 }
--- app/src/main/java/org/qr/barcode/scan/lens/extensions/NumberExtensions.kt
+++ app/src/main/java/org/qr/barcode/scan/lens/extensions/NumberExtensions.kt
@@ -4,3 +4,4 @@ import android.content.res.Resources
 
 
 fun Int.dpToPx() = (Resources.getSystem().displayMetrics.density * this).toInt()
+fun Float.dpToPx() = Resources.getSystem().displayMetrics.density * this
