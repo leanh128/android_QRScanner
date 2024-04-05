52a5c75e2c3bcade44ffaac33412431d1f5b586a
lee <anh.le.tuan@ntq-solution.com.vn>
2024-04-05 15:42:18 +0700

    build: release 3.0(10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vn.qrscanner"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 10
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
